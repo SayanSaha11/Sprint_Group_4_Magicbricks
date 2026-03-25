b62f4b01fe374e298f1bf3154488f66e611e03e6
Tanistha Panda <121510443+Tanistha2003@users.noreply.github.com>
2026-03-25 23:22:22 +0530

    Day_3 Deliverables

--- Day_3_Deliverables/MagicBricks_Sprint_Group4/src/test/java/runnerFile_Tanistha/AgentDetail.java
+++ Day_3_Deliverables/MagicBricks_Sprint_Group4/src/test/java/runnerFile_Tanistha/AgentDetail.java
@@ -1,5 +1,13 @@
 package runnerFile_Tanistha;
 
-public class AgentDetail {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
+@CucumberOptions(features= {"src/test/java/featureFile_Tanistha/AgentContact.feature"},
+glue="stepDefinition",
+dryRun=true)
+//dryRun=true when we don't have implementations yet in stepDefinition
+
+public class AgentDetail extends AbstractTestNGCucumberTests{
+	
 }
