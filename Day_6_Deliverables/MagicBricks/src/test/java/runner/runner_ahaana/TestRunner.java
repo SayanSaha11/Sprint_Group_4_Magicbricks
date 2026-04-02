6a921a493a5e61b0dbc6b6367dad760ad7c2b8f6
Aahana08 <aahanachowdhury2003@gmail.com>
2026-04-02 10:11:57 +0530

    Add files via upload

---
+++ Day_6_Deliverables/MagicBricks/src/test/java/runner/runner_ahaana/TestRunner.java
@@ -0,0 +1,18 @@
+package runner_ahaana;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = "StepDefinitions",
+    monochrome = true,
+    plugin = {
+        "pretty",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+    }
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+ 
+}
